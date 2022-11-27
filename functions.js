3a706bc978740de4794a9e43ead80c732f0c8ae1
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-11-27 17:00:40 +0600

    learning functions on node

---
+++ functions.js
@@ -0,0 +1,14 @@
+// normal function statement
+function sayHi(){
+    console.log('hello');
+}
+
+sayHi();
+
+// anonymous function
+
+sayBye = function(){
+    console.log("ok bye");
+}
+
+sayBye();
