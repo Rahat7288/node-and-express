1d73d1cbf7312f6d0058781d208e63b85adbc5cf
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-11-28 00:19:48 +0600

    learning about events and utils

--- eventEmitter/app.js
+++ eventEmitter/app.js
@@ -1,9 +1,29 @@
 var events = require('events');
+var util = require('util');
+// var myEmitter = new events.EventEmitter();
 
-var myEmitter = new events.EventEmitter();
+// myEmitter.on('someEvent', function(msg){
+//     console.log(msg);
+// });
 
-myEmitter.on('someEvent', function(msg){
-    console.log(msg);
-});
+// myEmitter.emit('someEvent', 'the event was emitted');
+var person = function (name){
+this.name = name;
+}
 
-myEmitter.emit('someEvent', 'the event was emitted');
+util.inherits(person,events.EventEmitter);
+
+var rahat = new person('Rahat');
+var islam = new person('Islam');
+var akash = new person('Akash');
+
+var people = [rahat,islam,akash];
+
+people.forEach(function(person){
+    person.on('speak',function(msg){
+        console.log(person.name + ' said ' + msg);
+    })
+})
+
+rahat.emit ('speak', 'hello world');
+
