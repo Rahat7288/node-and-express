ee0fea9919abb6a8e3c4e96395343be342bd3459
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-11-27 16:44:24 +0600

    using the global objects

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-console.log('hello mf');
+console.log('hello Rahat');
+
+// set a time to execute the function
 
 setTimeout(function(){
     console.log('3 seconds have passed');
 }, 3000);
+
+// setting an inerval time 
+
+var time = 0;
+setInterval(function(){
+    time +=2;
+    console.log(time +'time have passed');
+    if(time >10){
+        clearInterval(time);
+    }
+},2000);
