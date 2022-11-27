c47d352084e0a14b0c096eb0d7cd82f23bebeeca
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-11-27 23:07:05 +0600

    learning about modules and requirs

---
+++ Modules and Requir/count.js
@@ -0,0 +1,7 @@
+// declearing the function 
+// 
+var counter = function(arr){
+    console.log('there are '+ arr.length + ' elemets here');
+}
+// passing this function to another page
+module.exports = counter;
---
+++ Modules and Requir/mod.js
@@ -0,0 +1,5 @@
+// calling the function from another page
+var counter = require('./count');
+
+// declearing the function in the console 
+console.log(counter(['rahat','islam','akash']));
