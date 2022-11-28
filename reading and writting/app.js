755a671e0980a62d5a7406ef648caf6c928aab1b
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-11-28 20:11:58 +0600

    learning reading and writing in node js

--- reading and writting/app.js
+++ reading and writting/app.js
@@ -1,5 +1,15 @@
 var fs = require('fs');
-// reading files using universal text format 8
-var readMe = fs.readFileSync('note.txt', 'utf8');
+// reading files using universal text format 8 with syncro type 
+fs.readFile('note.txt', 'utf8', function(err, data){
+    // console.log(data);
+    // creating and writing in new file 
+    fs.writeFile('write.txt', data);
+});
+// to write in txt file using node;
+// fs.writeFileSync('note.txt', readMe);
 
-console.log(readMe);
+
+
+
+
+// console.log('test');
