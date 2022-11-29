228381a487c647d8e33fbd88b4704888bcad9140
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-11-29 18:13:57 +0600

    learning about the writestream on node

--- streamNdBuffoer/app.js
+++ streamNdBuffoer/app.js
@@ -1,8 +1,10 @@
 var fs = require('fs');
 
 var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+var myWriteStream = fs.createWriteStream(__dirname + '/write.txt');
 
 myReadStream.on('data' , function(chunk){
     console.log('new chunk recived:  ');
-    console.log(chunk);
+    // console.log(chunk);
+    myWriteStream.write(chunk);
 });
