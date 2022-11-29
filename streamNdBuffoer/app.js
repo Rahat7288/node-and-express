96f0490fed279f784cf62a33f3ef4ab831d9c5fb
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-11-29 18:05:41 +0600

    creating stream with node

---
+++ streamNdBuffoer/app.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+
+var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+
+myReadStream.on('data' , function(chunk){
+    console.log('new chunk recived:  ');
+    console.log(chunk);
+});
