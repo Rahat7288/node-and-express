159fa9c3dfc48b0055c59d10695029823f958e62
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2022-11-29 23:33:54 +0600

    learning more about pipe and strem and buffer

--- pipes/app.js
+++ pipes/app.js
@@ -1,7 +1,16 @@
-const { clear } = require('console');
+var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/write.txt');
+var myServer = http.createServer(function(req, res){
+    console.log('A server is created on: ', req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var myReadStream = fs.createReadStream(__dirname + '/readMe.txt', 'utf8');
+    // var myWriteStream = fs.createWriteStream(__dirname + '/write.txt');
 
-myReadStream.pipe(myWriteStream);
+    myReadStream.pipe(res);
+    // res.end('hello ninja');
+});
+
+myServer.listen(3000, '127.0.0.1');
+console.log('new server has created: ');
+
